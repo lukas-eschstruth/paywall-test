d25ab6a75c53654fb96574a0dbe32b1d76aa84d7
Lukas Eschstruth (Brandung) <lukas.eschstruth@brandung.de>
2021-01-15 15:14:13 +0100

    refactor to separate js

---
+++ app.js
@@ -0,0 +1,15 @@
+const checkoutBtnId = 'checkoutBtn';
+
+const ppp = PAYPAL.apps.PPP({
+    "approvalUrl": "https://www.sandbox.paypal.com/checkoutnow?token=6VH52758FV646041B",
+    "placeholder": "paywall",
+    "mode": "sandbox",
+    "country": "DE",
+    "language": "de_DE",
+    "showLoadingIndicator": true,
+    "buttonLocation": "outside",
+    "disableContinue": checkoutBtnId,
+    "enableContinue": checkoutBtnId
+});
+
+document.getElementById(checkoutBtnId).addEventListener('click', () => PAYPAL.apps.PPP.doCheckout());
