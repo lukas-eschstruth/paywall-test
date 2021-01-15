5a5a0691157490b99d414ad665f0ea892400736d
Lukas Eschstruth (Brandung) <lukas.eschstruth@brandung.de>
2021-01-15 15:43:46 +0100

    detect webview js bridge

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const checkoutBtnId = 'checkout-btn';
+window.addEventListener("ns-bridge-ready", function (e) {
+    document.body.classList.remove('loading');
 
-const ppp = PAYPAL.apps.PPP({
-    "approvalUrl": "https://www.sandbox.paypal.com/checkoutnow?token=6VH52758FV646041B",
-    "placeholder": "paywall",
-    "mode": "sandbox",
-    "country": "DE",
-    "language": "de_DE",
-    "showLoadingIndicator": true,
-    "buttonLocation": "outside",
-    "disableContinue": checkoutBtnId,
-    "enableContinue": checkoutBtnId
+    PAYPAL.apps.PPP({
+        "approvalUrl": "https://www.sandbox.paypal.com/checkoutnow?token=6VH52758FV646041B",
+        "placeholder": "paywall",
+        "mode": "sandbox",
+        "country": "DE",
+        "language": "de_DE",
+        "showLoadingIndicator": true,
+        "buttonLocation": "outside",
+        "disableContinue": checkoutBtnId,
+        "enableContinue": checkoutBtnId
+    });
+
+    document.getElementById(checkoutBtnId).addEventListener('click', () => PAYPAL.apps.PPP.doCheckout());
 });
-
-document.getElementById(checkoutBtnId).addEventListener('click', () => PAYPAL.apps.PPP.doCheckout());
